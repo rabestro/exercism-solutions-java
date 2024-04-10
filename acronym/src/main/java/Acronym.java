366cc2e1d7843e791bccc938a75e3f2dc16feaba
Jegors Čemisovs <jegors_cemisovs@epam.com>
2024-04-10 17:23:37 +0300

    Update acronym extraction regex in Acronym.java
    The regex used for acronym extraction in the Acronym class of Acronym.java has been updated for improved clarity and readability. The new approach specifically identifies letter groups, rather than using a combination of special characters and letters. This change makes the regex more comprehensible, and likely to be more maintainable in the future.

--- acronym/src/main/java/Acronym.java
+++ acronym/src/main/java/Acronym.java
@@ -1,24 +1,18 @@
 import java.util.function.Supplier;
 import java.util.regex.Pattern;
 
-/**
- * The Acronym class represents an acronym generator that extracts acronyms from a given phrase.
- * It implements the Supplier interface to provide the generated acronym as a result.
- */
 record Acronym(String phrase) implements Supplier<String> {
-    private static final Pattern ACRONYM_EXTRACTION_PATTERN = Pattern.compile("\\b_?(\\w)[^-\\s]*[- ]*");
-    private static final String CAPTURE_GROUP_TEMPLATE = "$1";
+    private static final Pattern WORDS_PATTERN = Pattern.compile("""
+            \\P{Alpha}*                 # Match any number of non-letter characters at the start.
+            (?<FirstLetter>\\p{Alpha})  # Find the first letter, capturing it in FirstLetter.
+            [\\p{Alpha}']*              # Followed by any number of letters or apostrophes.
+            \\P{Alpha}*                 # Ending with any non-letter characters.
+            """, Pattern.COMMENTS);
 
-
-    /**
-     * Retrieves the acronym generated from the given phrase.
-     *
-     * @return The acronym extracted from the phrase, represented as uppercase letters.
-     */
     public String get() {
-        return ACRONYM_EXTRACTION_PATTERN
+        return WORDS_PATTERN
                 .matcher(phrase)
-                .replaceAll(CAPTURE_GROUP_TEMPLATE)
+                .replaceAll("${FirstLetter}")
                 .toUpperCase();
     }
 }
