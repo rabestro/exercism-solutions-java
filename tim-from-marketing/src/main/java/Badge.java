410cb814926febb33ecec48e9832d96b494f9c62
Jegors Čemisovs <jegors_cemisovs@epam.com>
2024-04-15 19:35:16 +0300

    Add functionality to print name badges for employees
    The implemented feature allows printing name badges for employees, new employees, and the owner of the company. Tests for the badge class have been added to validate this functionality. A build.gradle file was created for project building, and README.md was updated to introduce the nullability in Java.

---
+++ tim-from-marketing/src/main/java/Badge.java
@@ -0,0 +1,15 @@
+class Badge {
+    public String print(Integer id, String name, String department) {
+        var sb = new StringBuilder();
+        if (id != null) {
+            sb.append("[").append(id).append("] - ");
+        }
+        sb.append(name).append(" - ");
+        if (department != null) {
+            sb.append(department.toUpperCase());
+        } else {
+            sb.append("OWNER");
+        }
+        return sb.toString();
+    }
+}
