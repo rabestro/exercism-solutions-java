f8e4188c8c12e717b0f6f0ceefd585a8f9694bce
Jegors Čemisovs <jegors_cemisovs@epam.com>
2024-04-21 10:46:47 +0300

    Refactor PigLatinTranslator regex pattern
    The existing regular expression in the PigLatinTranslator class has been refactored for clarity. The changes improve the readability and understanding of regex matching rules for consonants and word base. This commit makes the code more maintainable and easier to understand.

--- pig-latin/src/main/java/PigLatinTranslator.java
+++ pig-latin/src/main/java/PigLatinTranslator.java
@@ -3,12 +3,11 @@ import java.util.regex.Pattern;
 public final class PigLatinTranslator {
 
     private static final Pattern WORD_PATTERN = Pattern.compile("""
-            (?<consonants>                  # Define possible consonants
-                (?!xr|yt)                   # Consonants should not start with "xr" or "yt"
-                y?(qu|[\\w&&[^aeiouy]])*    # The main definition of a consonant group.
-            )?                              # The consonants group is optional.
-                                            # It is empty if a word starts with a vowel
-            (?<base>\\w+)                   # The definition of a base, unmoved groups of letters
+            (?<consonants>                  # Capture group for possible consonants at the start of a word
+                (?!xr|yt)                   # Look ahead negative. Consonants should not start with "xr" or "yt"
+                y?(qu|[\\w&&[^aeiouy]])*    # Allows optional 'y' at start, then 'qu' or any consonant
+            )?                              # The entire consonant group is optional (for words starting with vowels)
+            (?<base>\\w+)                   # Capture group for the base of the word (remaining letters)
             """, Pattern.COMMENTS);
     private static final String PIG_LATIN_FORMAT = "${base}${consonants}ay";
 
