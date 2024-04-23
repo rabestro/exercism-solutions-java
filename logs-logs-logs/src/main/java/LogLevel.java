65c78c0478fccf10862695ce3e129ce2fe79c461
Jegors Čemisovs <jegors_cemisovs@epam.com>
2024-04-23 19:50:55 +0300

    Add logging capabilities with different log levels
    New classes 'LogLevel' and 'LogLine' were created for handling logs with different levels. Enum 'LogLevel' supports six different levels. The 'LogLine' class includes methods for parsing log levels and formatting log lines. Also, Gradle configuration files were added.

---
+++ logs-logs-logs/src/main/java/LogLevel.java
@@ -0,0 +1,19 @@
+public enum LogLevel {
+    UNKNOWN(0),
+    TRACE(1),
+    DEBUG(2),
+    INFO(4),
+    WARNING(5),
+    ERROR(6),
+    FATAL(42);
+
+    private final int level;
+
+    LogLevel(int level) {
+        this.level = level;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+}
